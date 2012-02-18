ef4e964ac5eb536c7287b58e8c28c4e96edacf3c
plumppapaya <ylw3@cornell.edu>
2012-02-18 13:41:26 -0500

    Adding main classes

---
+++ controller.java
@@ -0,0 +1,21 @@
+//Main controller class. Handles communication between data servers and clients
+
+class Controller {
+
+	private initializeData(){
+		
+	}
+	
+	private spawnSocketListener(){
+		
+	}
+
+	public static void main(args[]){
+		initializeData();
+		spawnSocketListener():
+		
+		while(True){
+		}
+	}
+
+}
