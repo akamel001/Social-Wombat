05f92f9f14dadc701ec23caff0a1594e456d9845
Abdelrahman Kamel <ak883@cornell.edu>
2012-02-18 15:44:11 -0500

    Adding Client code

---
+++ Client.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Client {
+
+    public void run() {
+        
+        Scanner inStream = new Scanner(System.in);
+         
+        int input;
+
+        System.out.println("============================");
+        System.out.println("|   MENU SELECTION         |");
+        System.out.println("============================");
+        System.out.println("| Options:                 |");
+        System.out.println("|        1. Regsiter       |");
+        System.out.println("|        2. Log-in         |");
+        System.out.println("|        3. Exit           |");
+        System.out.println("============================");
+        
+        input = inStream.nextInt();
+        
+     
+        // Switch construct
+        switch (input) {
+            case 1:
+                System.out.println("Option 1 selected");
+                break;
+            case 2:
+                System.out.println("Option 2 selected");
+                break;
+            case 3:
+                System.out.println("Exit selected");
+                break;
+            default:
+                System.out.println("Invalid selection");
+                break; // This break is not really necessary
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Client instance = new Client();
+        instance.run();
+    }
+}
