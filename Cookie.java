4ddadf4b848a6e53ee7a83a72db9c1b5d6828c6d
Chris D <cgd39@cornell.edu>
2012-02-25 17:06:47 -0500

    Added comments to Cookie.java.

--- Cookie.java
+++ Cookie.java
@@ -1,19 +1,36 @@
 import java.util.Calendar;
 
+/**
+ * Cookie objects hold session info.
+ * @author chris
+ *
+ */
 public class Cookie{
 	private final Calendar expTime;
 	private final String sessionKey;
 		
+	/**
+	 * Creates a cookie with a passed session key.
+	 * @param s
+	 */
 	public Cookie(String s){
 		expTime = Calendar.getInstance();
 		expTime.add(Calendar.MINUTE, 10);
 		sessionKey = s;
 	}
 	
+	/** Returns the session key for the cookie.
+	 * 
+	 * @return
+	 */
 	public String getKey(){
 		return sessionKey;
 	}
 	
+	/** Returns true if the cookie's timestamp is greater than the current time.
+	 * 
+	 * @return boolean
+	 */
 	public boolean isValid(){
 		Calendar now = Calendar.getInstance(); 
 		return now.before(expTime);
