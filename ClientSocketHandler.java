4ceba34381e8a1ec769b30ecb7a7ceecaecdd8b8
Abdelrahman Kamel <ak883@cornell.edu>
2012-02-26 17:45:17 -0500

    Adding changes and syncing

--- ClientSocketHandler.java
+++ ClientSocketHandler.java
@@ -6,19 +6,19 @@ import java.util.Date;
 
 
 public class ClientSocketHandler {
-	
+
 	private static final int SERVER_PORT = 4444;
 
 
-	
+
 	public static Message constructMessage(String uName, Message.MessageType type){
-		
+
 
 		Message message = new Message();
 		message.setType(type);
 		message.setBody(uName);
 		message.setCode(0); // <- decide what codes are...?
-		
+
 		try {
 			//TODO change to static variables uses localhost for now
 			message.setRecipient(InetAddress.getLocalHost());
@@ -26,55 +26,54 @@ public class ClientSocketHandler {
 		} catch (UnknownHostException e) {
 			System.out.println(e);
 		}
-		
+
 		return message;
 	}
-	
+
 	public Message sendReceive(String uName, Message.MessageType type){
-		
+
 		Message messageSending = constructMessage(uName, type);
-		
+
 		//TODO construct the received message from the hub
 		Message messageReceived = null; 
 		Socket socket = null;
-	    ObjectOutputStream oos = null;
-	    ObjectInputStream ois = null;
-	    
+		ObjectOutputStream oos = null;
+		ObjectInputStream ois = null;
+
 		try{
-		     socket = new Socket(InetAddress.getLocalHost(), SERVER_PORT);
-		     
-		     // open I/O streams for objects
-		     oos = new ObjectOutputStream(socket.getOutputStream());
-		     ois = new ObjectInputStream(socket.getInputStream());
-		     
-//		     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-//		     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			 
-			 System.out.println("sent waiting for hub to respond...");
-		     //oos.writeObject(messageSending);
-			 oos.writeObject(new Date());
-			 oos.flush();
-			 
-		       // read an object from the server
-		        Date date = (Date) ois.readObject();
-		        System.out.print("The date is: " + date);
-		        oos.close();
-		        ois.close();
-		        
-		   } catch (UnknownHostException e) {
-			   System.out.println("Unknown host: " + messageSending.getRecipient());
-		     	System.exit(1);
-		   } catch  (IOException e) {
-			   System.out.println("No I/O");
-			   e.printStackTrace();
-			   System.exit(1);
-		   } catch (ClassNotFoundException e) {
+			socket = new Socket(InetAddress.getLocalHost(), SERVER_PORT);
+
+			// open I/O streams for objects
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			ois = new ObjectInputStream(socket.getInputStream());
+
+			// sending an object to the server
+			System.out.println("Sending message to server...");
+			oos.writeObject(messageSending);
+			oos.flush();
+
+			// read an object from the server
+			System.out.println("Receiving message from server...");
+			messageReceived = (Message) ois.readObject();
+			System.out.print(messageReceived.getBody());
+
+
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: " + messageSending.getRecipient());
+			System.exit(1);
+		} catch  (IOException e) {
+			System.out.println("No I/O");
+			e.printStackTrace();
+			System.exit(1);
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
-			   e.printStackTrace();
-		   } finally {
-			   
-		   }
-			
+			e.printStackTrace();
+		} finally {
+			oos.close();
+			ois.close();
+			socket.close();
+		}
+
 		return messageReceived;
 	}
 }
