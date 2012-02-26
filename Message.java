32cdfd4bbbaf908ba4216b479047baaa27e6c71f
chris d <cgd39@cornell.edu>
2012-02-25 19:14:40 -0500

    Updated .gitignore to ignore javadocs.

--- Message.java
+++ Message.java
@@ -2,55 +2,22 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class Message implements Serializable{
+public class Message{
 	private InetAddress sender;
 	private InetAddress recipient;
 	private String body;
 	private MessageType type;
 	private Cookie cookie;
 	private int code;
-	private String classroom_id;
 	
 	public enum MessageType {
-	    // Client -> Hub only
-		Client_LogIn, 
-	    Client_GetClassEnrollment,
-	    Client_GetUserEnrollment,
-	    Client_SetPermissions,	//of users in a class
-	    Client_GetEnrollmentList,
-	    Client_DeleteSelf,
-	    Client_RequestEnrollment,
-	    
-	    // Client -> Hub -> Server
-	    Client_CreateClassroom,
-	    Client_CreatePost,
-	    Client_CreateComment,
-	    Client_GoToClassroom,
-	    Client_GoToThread,	//also known as post
-	    Client_DeleteClassroom,
-	    Client_DeleteThread,	    
-	    
-	    // Server -> Hub -> Client
-	    Server_A,
-	    Server_B
+	    Client_Authentication, Client_B, Server_A, Server_B 
 	}
 	
 	public Message(){
 		//
 	}
-	
-	/* This is neccessary for message forwarding because the client doesn't
-	 * know which server its message should be sent to. So the Hub does the job 
-	 * of mapping the classroom to an appropriate server!
-	 */
-	public String getClassroom_ID() {
-		return classroom_id; 
-	}
-	
-	public void setClassroom_ID(String classroom_id) {
-		this.classroom_id = classroom_id;
-	}
-	
+
 	public InetAddress getSender() {
 		return sender;
 	}
@@ -98,6 +65,4 @@ public class Message implements Serializable{
 	public void setCode(int code) {
 		this.code = code;
 	}
-
-	
 }
