e961688a6522c3037290585085763151ee0a6c63
Abdelrahman Kamel <ak883@cornell.edu>
2012-02-26 21:45:08 -0500

    new code

--- Client.java
+++ Client.java
@@ -99,7 +99,7 @@ public class Client {
 	}
 	
 	public static void main(String [] args) {
-		handleRegister("bob");
+		handleLogin("bob");
 	}
 
 }
--- ClientSocketHandler.java
+++ ClientSocketHandler.java
@@ -9,14 +9,12 @@ public class ClientSocketHandler {
 
 
 
-	public static Message constructMessage(String uName, Message.MessageType type, String body){
+	public static Message constructMessage(String uName, Message.MessageType type, Object body){
 
 
 		Message message = new Message();
 		message.setType(type);
 		message.setBody(body);
-		message.setCode(0); // <- decide what codes are...?
-		
 
 		try {
 			//TODO change to static variables uses localhost for now
@@ -28,10 +26,57 @@ public class ClientSocketHandler {
 
 		return message;
 	}
-
+	
 	public Message sendReceive(String uName, Message.MessageType type){
 
-		Message messageSending = constructMessage(uName, type, "");
+		Message messageSending = constructMessage(uName, type, null);
+
+		//TODO construct the received message from the hub
+		Message messageReceived = null; 
+		Socket socket = null;
+		ObjectOutputStream oos = null;
+		ObjectInputStream ois = null;
+
+		try{
+			socket = new Socket(InetAddress.getLocalHost(), SERVER_PORT);
+
+			// open I/O streams for objects
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			ois = new ObjectInputStream(socket.getInputStream());
+
+			// sending an object to the server
+			System.out.println("Sending message to server...");
+			oos.writeObject(messageSending);
+			oos.flush();
+
+			// read an object from the server
+			System.out.println("Receiving message from server...");
+			messageReceived = (Message) ois.readObject();
+			System.out.print(messageReceived.getBody());
+			
+			oos.close();
+			ois.close();
+			socket.close();
+
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: " + messageSending.getRecipient());
+			System.exit(1);
+		} catch  (IOException e) {
+			System.out.println("No I/O");
+			e.printStackTrace();
+			System.exit(1);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+		return messageReceived;
+	}
+	
+
+	public Message sendReceive(String uName, Message.MessageType type, Object body){
+
+		Message messageSending = constructMessage(uName, type, body);
 
 		//TODO construct the received message from the hub
 		Message messageReceived = null; 
