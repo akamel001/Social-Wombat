224bc3f910890a4d8d14c7cf1a92fd4aacc5f610
chris d <cgd39@cornell.edu>
2012-02-26 21:36:23 -0500

    Added ClassDB.java and ClassRoom.java.

---
+++ ClassDB.java
@@ -0,0 +1,109 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * 
+ */
+public class ClassDB {
+	private Map <String, ClassRoom> classRoomList; 
+	private int nextClass = 1;
+	
+	/**
+	 * ClassDB holds a list of ClassRoom objects.
+	 */
+	public ClassDB(){
+		classRoomList = Collections.synchronizedMap(new HashMap<String, ClassRoom>());
+	}
+	
+	/**
+	 * Adds a classroom to the DB.
+	 * @param n The name of the new classroom -- must be unique!
+	 * @return Returns 1 if the class was added, -1 if it already exists.
+	 */
+	public int addClassRoom(String n){
+		if (classRoomList.containsKey(n))
+			return -1;
+		else{
+			ClassRoom c = new ClassRoom(n, nextClass);
+			classRoomList.put(n, c);
+			return 1;
+		}
+	}
+	
+	/**
+	 * Removes a class from the DB.
+	 * @param c The name of the class.
+	 * @return Returns 1 if the class was removed, -1 if the class did not exist.
+	 */
+	public int removeClassRoom(String c){
+		ClassRoom t = classRoomList.remove(c);
+		if (t==null)
+			return -1;
+		else
+			return 1;
+	}
+	
+	/**
+	 * Adds a post to a classroom.
+	 * @param className The classname to which the post is to be added.
+	 * @param postTitle The title of the post to be added.
+	 * @param postBody The body of the post to be added.
+	 * @return Returns 1 if the post was added, -1 if it was not.
+	 */
+	public int addPost(String className, String postTitle, String postBody){
+		if (className==null || postTitle==null || postBody==null)
+			return -1;
+		ClassRoom c = classRoomList.get(className);
+		if (c==null)
+			return -1;
+		if (postTitle.length()<1 || postTitle.length()> Post.maxTitleLength)
+			return -1;
+		if(postBody.length()<1 || postBody.length()>Post.maxCommentLength)
+			return -1;
+		int out = c.addNewPost(postTitle, postBody);
+		return out;
+	}
+	
+	/**
+	 * Removes a post from a class.
+	 * @param className The name of the class that removes the post.
+	 * @param postId The name of the post to be removed.
+	 * @return Returns 1 if the post was removed, -1 if was not contained in the classroom.
+	 */
+	public int removePost(String className, int postId){
+		ClassRoom c = classRoomList.get(className);
+		if (c==null)
+			return -1;
+		int out = c.removePost(postId);
+		return out;
+	}
+	
+	/**
+	 * Adds s comment to a post in a classroom
+	 * @param className The name of the classroom that contains the post.
+	 * @param postId The name of the post to which to add the comment.
+	 * @param comment The comment to be added.
+	 * @return Returns 1 if the comment had been added, -1 otherwise. 
+	 */
+	public int addComment(String className, int postId, String comment){
+		if (className==null || comment==null)
+			return -1;
+		ClassRoom c = classRoomList.get(className);
+		int out = c.addComment(postId, comment);
+		return out;
+	}
+	
+	/**
+	 * Removes a comment from a post in a classroom.
+	 * @param className The name of the classroom which contains the post. 
+	 * @param postId The id number of the post which contains the comment.
+	 * @param commentId The id number of the comment which is to be removed.
+	 * @return Returns 1 if the comment is removed, -1 if the id did not exist in the post.
+	 */
+	public int removeComment(String className, int postId, int commentId){
+		ClassRoom c = classRoomList.get(className);
+		int out = c.removeComment(postId, commentId);
+		return out;
+	}
+}
---
+++ ClassRoom.java
@@ -0,0 +1,114 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ClassRoom {
+	private static final int maxNameLength = 30;
+	private int nextPost = 1;
+	private int idNum;
+	private String name;
+	private Map <Integer, Post> postList;
+
+	
+	/**
+	 * Default constructor disallowed.
+	 */
+	private ClassRoom(){}
+	
+	/**
+	 * Creates a new classroom with the given name.
+	 * @param n 
+	 */
+	protected ClassRoom(String n, int i){
+		if (n.length()>maxNameLength)
+			n = n.substring(0, maxNameLength-1);
+		name = n;
+		if (n.length()==0)
+			n = "[no class title]"; // Should be caught at ClassDB
+		idNum = i;
+		postList = Collections.synchronizedMap(new HashMap<Integer, Post>());
+	}
+	
+	/**
+	 * Creates a new post and adds it to the post list.
+	 * @param postTitle The title of the post. The length must be 1<=t<=Post.maxTitleLength.
+	 * @param postBody  The body of the post. The length must be 1<=b<=Post.maxBodyLength.
+	 * @return
+	 */
+	protected int addNewPost(String postTitle, String postBody){
+		if (postTitle == null || postBody == null)
+			return -1;
+		if (postTitle.length() > Post.maxTitleLength || postTitle.length() <1)
+			return -1;
+		if (postBody.length() > Post.maxCommentLength || postBody.length() <1)
+			return -1;
+		Post p = new Post(postTitle, postBody, nextPost);
+		synchronized (this) {
+			postList.put(nextPost, p);
+			nextPost++;
+		}
+		return 1;
+	}
+	
+	/**
+	 * Removes a post from the post and all its comments from the post list.
+	 * @param p The id of the post to be removed.
+	 * @return Returns 1 if the post was successfully removed, -1 otherwise.
+	 */
+	protected int removePost(int p){
+		postList.remove(p);
+		if (postList==null)
+			return -1;
+		else
+			return 1;
+	}
+	
+	/**
+	 * Adds a comment to a post.
+	 * @param postId The id number of the post to which the comment will be added.
+	 * @param comment The comment to be added.
+	 * @return
+	 */
+	protected int addComment(int postId, String comment){
+		Post p = postList.get(postId);
+		int out = p.addComment(comment);
+		return out;
+	}
+	
+	/**
+	 * Removes a comment from a post.
+	 * @param postId The id number of the post to which to add a comment. 
+	 * @param commentId The id number of the comment to be removed.
+	 * @return Returns 1 if the comment is removed, -1 if the comment was not in the post.
+	 */
+	protected int removeComment(int postId, int commentId){
+		Post p = postList.get(postId);
+		int out = p.removeComment(commentId);
+		return out;
+	}
+	
+	/**
+	 * Returns the post list for a class.
+	 * @return Returns a map (Integer,String) containing all the posts for the class.
+	 */
+	protected Map<Integer, Post> getClassRoom(){
+		return postList;
+	}
+	
+	/**
+	 * Returns the id number of the class.
+	 * @return Returns the value of the idNum field.
+	 */
+	protected int getId(){
+		return idNum;
+	}
+	
+	/**
+	 * returns the name of the class.
+	 * @return
+	 */
+	protected String getName(){
+		return name;
+	}
+}
