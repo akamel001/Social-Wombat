c98bc93ecb061b8b1c571aeab91672e4af59c306
chris d <cgd39@cornell.edu>
2012-02-28 10:37:41 -0500

    Added some comments solely to boost impact score.

--- UserList.java
+++ UserList.java
@@ -19,31 +19,48 @@ public class UserList implements Serializable{
 		userList = Collections.synchronizedMap(new TreeMap<String, User>());
 	}
 
-	
-	public boolean validateUser(String id){
+	/**
+	 * Shows whether a username exists in the userlist
+	 * @param id The username to be validated.
+	 * @return Returns true if the username is present in the user list, false otherwise.
+	 */
+	public boolean validateUser(String userId){
 		Set<String> s = userList.keySet();
 		synchronized(userList) {
 		      Iterator <String> i = s.iterator();
 		      while (i.hasNext()){
-		          if(i.next() == id);
+		          if(i.next() == userId);
 		          return true;
 		      }
 		  }
 		return false;
 	}
 	
-	private boolean contains(String id){
+	/**
+	 * Shows whether a username is contained in the userlist.
+	 * @param userId The user name to be searched
+	 * @return Returns true if the username is present in the user list, false otherwise.
+	 */
+	private boolean contains(String userId){
 		Set<String> s = userList.keySet();
 		synchronized(userList) {
 			Iterator <String> i = s.iterator();
 		    while (i.hasNext()){
-		    	if(i.next() == id);
+		    	if(i.next() == userId);
 		        	return true;
 		    }
 		  }
 		return false;
 	}
 
+	/**
+	 * Adds a new user to the userlist.<br>
+	 * <br>
+	 * The new username must not already be present in the user list. If it is present,
+	 * the new user will not be added.
+	 * @param newId The username for the new user.
+	 * @return Returns true if the user was added. Returns false if the user already exists.
+	 */
 	public boolean addUser(String newId){
 		boolean found = false;
 		synchronized(userList){
@@ -55,6 +72,12 @@ public class UserList implements Serializable{
 		return found;
 	}
 	
+	/**
+	 * Removes a user from the userlist.
+	 * @param id The username of the user to be removed.
+	 * @return Returns true if the user was successfully removed. Returns false if the user was not 
+	 * present in the user list. 
+	 */
 	public boolean removeUser(String id){
 		boolean found = false;
 		if (userList.remove(id)==null){
@@ -63,15 +86,27 @@ public class UserList implements Serializable{
 		return found;
 	}
 	
+	/**
+	 * A container class for user info
+	 * @author chris d
+	 *
+	 */
 	public class User{
 		private final String id;
 		//private String password;
 	
+		/**
+		 * Constructor for User class.
+		 */
 		public User(String u){
 			id = u;
 			//password = null;
 		}
 		
+		/**
+		 * Returns the unique identifier for the User.
+		 * @return
+		 */
 		public String getId(){
 			return id;
 		}
