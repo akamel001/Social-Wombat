9c94f10e95934d2ab19a8788c06b070bd13bfdcc
plumppapaya <ylw3@cornell.edu>
2012-02-28 22:21:55 -0500

    debug code

--- HubSocketHandler.java
+++ HubSocketHandler.java
@@ -178,6 +178,7 @@ public class HubSocketHandler extends Thread{
 						msg.setCode(1);
 						System.out.println("User " + msg.getCookie().getKey() + " logged in");
 					} 
+					System.out.println("Message code returned is: " + msg.getCode());
 					returnMessage(msg);
 					break;
 				// Returns in body all users in a classroom
