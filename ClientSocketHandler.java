981f110568531b57f4786de737fa3c8fbbbe6d77
Abdelrahman Kamel <ak883@cornell.edu>
2012-02-28 21:32:15 -0500

    small bug fix with with client socket handler

--- ClientSocketHandler.java
+++ ClientSocketHandler.java
@@ -7,8 +7,8 @@ public class ClientSocketHandler {
 
 	private static final int HUB_PORT = 4444;
 
-	private static Message messageSending = null;
-	private static Message messageReceived = null; 
+	private static Message messageSending = new Message();
+	private static Message messageReceived = new Message(); 
 	private static Socket socket = null;
 	private static ObjectOutputStream oos = null;
 	private static ObjectInputStream ois = null;
