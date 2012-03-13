d00ff54b0cb0085d8b0336046d5dad41054a434c
chris d <cgd39@cornell.edu>
2012-03-13 00:21:30 -0400

    Encryptionized ServerList.java.

--- ServerList.java
+++ ServerList.java
@@ -9,18 +9,21 @@ import java.util.Iterator;
 /**
  * ServerList holds info and access methods for server info.
  * 
- * @author chris
+ * @author chris d
  *
  */
-public class ServerList implements Serializable{
-
+public final class ServerList implements Serializable{
+	
 	private static final long serialVersionUID = -5018052157599076338L;
 	private Map<Integer, ServerData> serverList;
 	private int nextId; 
 	
+	/**
+	 * Creates a list of servers. First server created has an id of 1.
+	 */
 	public ServerList(){
 		serverList = Collections.synchronizedMap(new TreeMap<Integer, ServerData>());
-		nextId = 0;
+		nextId = 1; // first server created is #1
 	}
 
 	/**
@@ -31,10 +34,13 @@ public class ServerList implements Serializable{
 	 * NOTE: two ports cannot have both the same ip and port.
 	 * @param ip The ip address of the server.
 	 * @param port The port number of the server.
+	 * @param pass The password for the server. NOTE: the passed char[] is zeroed out as soon as the password is set! 
+	 * It cannot be used again!!!!
+	 * @param encrypter An AES object used to encrypt the password.
 	 * @return Returns the server id or -1 if the ip/port combination already exists in the list.
 	 */
-	public int addServer(InetAddress ip, int port){
-		if(ip==null || port<0 || port>49150)
+	public int addServer(InetAddress ip, int port, char[] pass, AES encrypter){
+		if(ip==null || port<0 || port>49150 || encrypter==null)
 			return -1;
 		
 		// check to see if ip/port combo exists
@@ -49,14 +55,18 @@ public class ServerList implements Serializable{
 					return -1;
 				}
 			}
-			nextId ++;
-			ServerData s = new ServerData();
-			s.setId(nextId);
+			ServerData s = new ServerData(nextId);
 			s.setAddress(ip);
 			s.setPort(port);
+			s.setPassword(encrypter.encrypt(pass));
+			// Zero out passed password
+			for(int j=0; j<pass.length; j++)
+				pass[j]='0';
 			serverList.put(nextId, s);
 		}
-		return nextId;
+		int out = nextId;
+		nextId++;
+		return out;
 	}
 
 	/**
@@ -96,19 +106,33 @@ public class ServerList implements Serializable{
 		else
 			return temp.getPort();
 	}
+	
+	
+	/**
+	 * Gives the index of the last server created
+	 * @return
+	 */
+	public int getLastServer(){
+		return nextId-1;
+	}
 
 	/**
 	 * Server class holds the info for a single server.
 	 * @author chris
-	 *
+	 * The only part of this class that is encrypted is the password.
 	 */
-	public class ServerData implements Serializable{
+	public final class ServerData implements Serializable{
 		
 		private static final long serialVersionUID = -2846614313431062929L;
-		private int id;
+		private final int id;
 		private InetAddress ip;
 		private int port;
+		private byte[] password;
 
+		
+		@SuppressWarnings("unused")
+		private ServerData(){id=-1;}
+		
 		/**
 		 * Creates a ServerList object with default values.<br>
 		 * <br>
@@ -117,8 +141,8 @@ public class ServerList implements Serializable{
 		 * 		ip = null<br>
 		 * 		port = -1<br>
 		 */
-		public ServerData(){
-			id = -1;
+		protected ServerData(int i){
+			id = i;
 			ip = null;
 			port = -1;
 		}
@@ -127,31 +151,15 @@ public class ServerList implements Serializable{
 		 * 
 		 * @return
 		 */
-		public int getId(){
+		protected int getId(){
 			return id;
-		}
-
-		/**
-		 * Sets the id for a server.<br>
-		 * <br>
-		 * Id CAN ONLY BE SET ONCE! If the id has already been set, it cannot be reset.
-		 * @param i
-		 * @return
-		 */
-		public int setId(int i){
-			if (i<0 || id>0)
-				return -1;
-			else{
-				id = i;
-				return 1;
-			}
 		}
 
 		/** Returns the ip address of the server.
 		 * 
 		 * @return
 		 */
-		public InetAddress getAddress(){
+		protected InetAddress getAddress(){
 			return ip;
 		}
 
@@ -162,7 +170,7 @@ public class ServerList implements Serializable{
 		 * @param i
 		 * @return Returns -1 if passed value is null, 1 on success.
 		 */
-		public int setAddress(InetAddress i){
+		protected int setAddress(InetAddress i){
 			if (i== null || ip!=null)
 				return -1;
 			else{
@@ -175,7 +183,7 @@ public class ServerList implements Serializable{
 		 * 
 		 * @return
 		 */
-		public int getPort(){
+		protected int getPort(){
 			return port;
 		}
 
@@ -185,7 +193,7 @@ public class ServerList implements Serializable{
 		 * @param p Must be 0>=p<=49151
 		 * @return
 		 */
-		public int setPort(int p){
+		protected int setPort(int p){
 			if (p<0 || p>49151)
 				return -1;
 			else{
@@ -193,13 +201,43 @@ public class ServerList implements Serializable{
 				return 1;
 			}
 		}
-	}
-	
-	/**
-	 * Gives the index of the last server created
-	 * @return
-	 */
-	public int getLastServer(){
-		return nextId;
+		
+		/**
+		 * Sets the password for the server. </br>
+		 * NOTE: this function zeros out the passed array.
+		 * @param p The new encrypted password. 
+		 * @return Returns 1 on success, which, hopefully, is always.
+		 */
+		protected int setPassword(byte[] p){
+			// zero out old password
+			if (password != null){
+				for (int i=0; i<password.length; i++)
+					password[i]='0';
+			}
+			// copy in new password
+			password = new byte[p.length];
+			for (int i=0; i<password.length; i++)
+				password[i]=p[i];
+			// Zero out passed byte array
+			for (int i=0; i<p.length; i++)
+				p[i]=0;
+			return 1;
+		}
+		
+		/**
+		 * Returns an encrypted copy of the server's password.
+		 * @return Returns a copy of the password. NOTE: you must zero put the returned array as soon as you are finished with it!!!!!! 
+		 */
+		protected byte[] getPassword(){
+			if (password==null)
+				return null;
+			else{
+				byte[] out = new byte[password.length];
+				for(int i=0; i<password.length; i++)
+					out[i]=password[i];
+				return out;
+			}
+		}
+		
 	}
 }
