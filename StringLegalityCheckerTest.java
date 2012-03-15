132ca04d0f65390a07948d387468d0871f121639
Julia Tazin <jtazin@gmail.com>
2012-03-14 20:44:31 -0400

    Fixed the regex! Woo! Took forever. Passes all tests now.

--- StringLegalityCheckerTest.java
+++ StringLegalityCheckerTest.java
@@ -30,17 +30,22 @@ public class StringLegalityCheckerTest {
 	public void testCheckIfPasswordStringIsLegal() {	
 		assertEquals(true, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver1#")); // legal string
 		assertEquals(true, StringLegalityChecker.checkIfPasswordStringIsLegal("ILoveComputer$cience3")); // legal string
-		assertEquals(true, StringLegalityChecker.checkIfPasswordStringIsLegal("yoshi54A!@#$%^&*_+-=")); // legal string
-		assertEquals(true, StringLegalityChecker.checkIfPasswordStringIsLegal("!@#$%^&*yo_+-=5S")); // legal string
-		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("A1#aaaa")); // too short
-		assertEquals(true, StringLegalityChecker.checkIfPasswordStringIsLegal("A1#aaaaa")); // legal string
-		assertEquals(true,  StringLegalityChecker.checkIfPasswordStringIsLegal("A1#aaaaaaaaaaaaaaaaaaaaaa")); // legal string
-//		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("A1#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")); // too long
+		assertEquals(true, StringLegalityChecker.checkIfPasswordStringIsLegal("yoshi54A@#$%^&+=")); // legal string
+		assertEquals(true, StringLegalityChecker.checkIfPasswordStringIsLegal("@#$%^&+=yo5S")); // legal string
+		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("A1#a5aa")); // too short (7 chars)
+		assertEquals(true,  StringLegalityChecker.checkIfPasswordStringIsLegal("A1#a5aaa")); // legal string (8 chars)
+		assertEquals(true,  StringLegalityChecker.checkIfPasswordStringIsLegal("A1#a5aaaa5aaaa5aaaa5aaaa5")); // legal string (25 chars)
+		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("A1#a5aaaa5aaaa5aaaa5aaaa5a")); // too long (26 chars)
 		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver#")); // not enough variety (missing numeral)
 		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("catluvver1#")); // not enough variety (missing uppercase letter)
 		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("CATLUVVER1#")); // not enough variety (missing lowercase letter)
-//		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver1")); // not enough variety (missing special character)
+		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver1")); // not enough variety (missing special character)
 		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver1#?")); // invalid character
+		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver1#?#")); // invalid character
+		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("?catLuvver1#")); // invalid character
+		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver1#?#")); // invalid character
+		assertEquals(false, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver1# #")); // invalid character
+		assertEquals(true, StringLegalityChecker.checkIfPasswordStringIsLegal("catLuvver1##")); // invalid character
 	}
 
 }
