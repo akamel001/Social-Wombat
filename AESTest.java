7afdd152ea3a59841d5a50ca5a723182f348ed3b
Abdelrahman Kamel <ak883@cornell.edu>
2012-03-15 00:20:48 -0400

    Semi working AES

--- AESTest.java
+++ AESTest.java
@@ -9,27 +9,44 @@ public class AESTest {
 	@Test
 	public void testAES() {
 		
-	    byte[] salt = new byte[8];
-	    SecureRandom random = new SecureRandom();
-		random.nextBytes(salt);
-		
-		String password = "secure";
-		
-		AES obj1 = new AES(password.toCharArray(), salt);
-		AES obj2 = new AES(password.toCharArray(), salt);
-		
-		Message message = new Message();
-		
-		message.setClassroom_ID("Class1");
-		message.setBody("1337 H4kr");
-		
-		byte[] enc = obj1.encrypt(message);
-		System.out.println("Message encrypted: \n====> " + enc);
-		Message decypredMessage = (Message) obj2.decryptObject(enc);
-		
-		
-		assertEquals("1337 H4kr", decypredMessage.getBody());
-		System.out.println(decypredMessage.getBody());
-	}
+//	    byte[] salt = new byte[8];
+//	    byte[] salt2 = new byte[8];
+//	    
+//	    SecureRandom random = new SecureRandom();
+//		random.nextBytes(salt);
+//		random.nextBytes(salt2);
+//
+//		String password = "secure";
+//		String password2 = "seucre";
+//		AES obj1 = new AES(password.toCharArray());
+//		
+//		AES obj2 = new AES(password.toCharArray(), obj1.getIv());
+//		
+//		Message message = new Message();
+//		
+//		message.setClassroom_ID("Class1");
+//		message.setBody("1337 H4kr");
+//		
+//		//String message = "I am 1337";
+//		//String message2 = "lolcatz";
+//		
+//		byte[] enc1 = obj1.encrypt(message);
+//		byte[] enc2 = obj2.encrypt(message);
+//		//byte[] enc2 = obj1.encrypt(message2);
+//
+//		//byte[] enc2 = obj2.encrypt(message);
+//
+//		//obj2.setIv(obj1.getIv());
+//		Message deMessage = (Message) obj1.decryptObject(enc1);
+//		Message deMessage2 = (Message) obj2.decryptObject(enc1);
+//
+//		System.out.println("Message encrypted: \n====> " + enc1);
+//		//System.out.println("Message encrypted: \n====> " + enc2);
+//
+//		System.out.println(deMessage.getBody());
+//		System.out.println(deMessage2.getBody());
+//
+//		//System.out.println(obj2.decrypt(enc1));
 
+	}
 }
