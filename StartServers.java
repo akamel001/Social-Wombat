48b33ed842b7f4594c8a2f4b0beae3f84852f0e6
plumppapaya <ylw3@cornell.edu>
2012-03-16 04:25:12 -0400

    wheeeeeeeeeeeeee

--- StartServers.java
+++ StartServers.java
@@ -9,13 +9,17 @@ public class StartServers {
 		Server s1 = new Server("s1","password".toCharArray());
 		System.out.println("IP of s1: " + s1.getIP());
 		s1.start();
+		
+		//We will avoid starting multiple servers on the same ip for 
+		//now to simplify things
+		/*
 		Server s2 = new Server("s2","password".toCharArray());
 		System.out.println("IP of s2: " + s2.getIP());
 		s2.start();
 		Server s3 = new Server("s3","password".toCharArray());
 		System.out.println("IP of s3: " + s3.getIP());		
 		s3.start();
-		
+		*/
 	}
 
 }
