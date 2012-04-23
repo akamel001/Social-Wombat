faa33f08c87dfa811e6b74c3459e99ccff1d5fe0
chris d <cgd39@cornell.edu>
2012-04-22 20:30:01 -0400

    Added exactly two characters to SysAdmininterface.java.

--- src/hub/SysAdminInterface.java
+++ src/hub/SysAdminInterface.java
@@ -69,7 +69,7 @@ public final class SysAdminInterface {
 	private static final String mSTART_HUB_SUCCESS =		UserInterfaceHelper.addFormattingAlignLeft("You have successfully start up the hub.");
 	private static final String mSHUT_DOWN_HUB_SUCCESS = 	UserInterfaceHelper.addFormattingAlignLeft("You have successfully shut down the hub.");
 	private static final String mADD_SERVER_SUCCESS = 		UserInterfaceHelper.addFormattingAlignLeft("You have successfully added a server.");
-	private static final String mCONNECT_SERVERS_MESSAGE_1 ="You have been successfully connect to ";
+	private static final String mCONNECT_SERVERS_MESSAGE_1 ="You have been successfully connected to ";
 	private static final String mCONNECT_SERVERS_MESSAGE_2 =" servers.";
 	
 	private static void systemLoginPage(String messages) {
